a5e52118970774f7ec0b6ba3ed452464e28234d3
unknown <justinasve@gmail.com>
2023-01-13 00:08:47 +0200

    Additional functionality

--- app/app.js
+++ app/app.js
@@ -65,9 +65,10 @@ app.post('/register', (req, res) => {
         'INSERT INTO users (name, password) VALUES (?, ?)',
         [name, hashedPassword],
         (err, result) => {
-            if (err.code === 'ER_DUP_ENTRY') {
+            if (err?.code === 'ER_DUP_ENTRY') {
                 res.sendStatus(400)
             }
+
             res.send(result);
         }
     )
