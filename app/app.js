7b18057c1af7af092aaf683a4697c242e0a8f939
unknown <justinasve@gmail.com>
2023-01-19 01:26:01 +0200

    Navigate user to expenses page if token in storage is valid

--- app/app.js
+++ app/app.js
@@ -51,7 +51,7 @@ app.get('/expenses', verifyToken, (req, res) => {
 });
 
 //12 uzduotis 15 skaidre
-app.post('/expenses', (req, res) => {
+app.post('/expenses', verifyToken, (req, res) => {
     const { type, amount, userId } = req.body;
 
     connection.execute(
@@ -100,7 +100,7 @@ app.post('/login', (req, res) => {
                 const isPasswordCorrect = bcrypt.compareSync(password, passwordHash);
                 if (isPasswordCorrect) {
                     const { id, name } = result[0];
-                    const token = jwt.sign({ id, name }, process.env.JWT_SECRET_KEY, { expiresIn: '5s'});
+                    const token = jwt.sign({ id, name }, process.env.JWT_SECRET_KEY);
                     res.send({ token, id, name });
                 } else {
                     res.sendStatus(401);
@@ -108,7 +108,17 @@ app.post('/login', (req, res) => {
             }
         }
     );
-})
+});
+
+app.get('/token/verify', (req, res) =>{
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        res.send(user);
+    } catch(e) {
+        res.send({ error: 'Invalid token' });
+    }
+});
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
